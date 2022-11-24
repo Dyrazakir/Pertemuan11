30724593d4737aa5dba61f8653f8c8d8f14fdbe5
Dyra Zaki R <114544204+Dyrazakir@users.noreply.github.com>
2022-11-24 18:52:20 +0700

    Add files via upload

---
+++ ProgramDAO/src/barang.java
@@ -0,0 +1,40 @@
+
+public class barang {
+    
+    private String kode_barang;
+    private String nama_barang;
+    private int jumlah;
+    private long harga;
+
+    public String getKode_barang() {
+        return kode_barang;
+    }
+
+    public void setKode_barang(String kode_barang) {
+        this.kode_barang = kode_barang;
+    }
+
+    public String getNama_barang() {
+        return nama_barang;
+    }
+
+    public void setNama_barang(String nama_barang) {
+        this.nama_barang = nama_barang;
+    }
+
+    public int getJumlah() {
+        return jumlah;
+    }
+
+    public void setJumlah(int jumlah) {
+        this.jumlah = jumlah;
+    }
+
+    public long getHarga() {
+        return harga;
+    }
+
+    public void setHarga(long harga) {
+        this.harga = harga;
+    }
+}
